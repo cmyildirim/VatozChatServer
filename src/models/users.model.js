0857e1ec243a865632705dc0347493e1d9b880f1
cmyildirim <cihangirmy@gmail.com>
2017-06-29 10:30:51 +0300

    Basic Auth

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,15 +1,17 @@
-// messages-model.js - A mongoose model
-// 
+// users-model.js - A mongoose model
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
-  const messages = new mongooseClient.Schema({
-    text: { type: String, required: true },
-    name: { type: String, required: true },
+  const users = new mongooseClient.Schema({
+  
+    email: {type: String, unique: true},
+    password: { type: String },
+
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
 
-  return mongooseClient.model('messages', messages);
+  return mongooseClient.model('users', users);
 };
