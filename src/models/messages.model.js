5fc4b12bae4872e12224b92fcf3ecc5a460156a3
cmyildirim <cihangirmy@gmail.com>
2017-07-01 16:45:01 +0300

    Commit before model changes

--- src/models/messages.model.js
+++ src/models/messages.model.js
@@ -7,6 +7,7 @@ module.exports = function (app) {
   const messages = new mongooseClient.Schema({
     text: { type: String, required: true },
     userId: {type: mongooseClient.Schema.ObjectId },
+    
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
